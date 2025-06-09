5a45f4ac9259b60bab91aa5f53374876d6349e7f
MoriahYen <moriahyen@gmail.com>
2025-06-09 11:25:27 +0800

    11. Locators supported by playwright and how to type into elements on page

--- playwright.config.js
+++ playwright.config.js
@@ -3,14 +3,15 @@ import { defineConfig, devices, expect } from '@playwright/test';
 
 const config = ({
   testDir: './tests',
-  timeout: 40*1000;
+  timeout: 40*1000,
   expect: {
-    timeout: 40*1000,
+    timeout: 5000,
   },
   reporter: "html",
 
   use: {
-    browserName: "chromium",
+    browserName: "webkit", //"firefox",//"chromium",
+    headless: true,
     
   },
 
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,15 +1,25 @@
-const {test} = require('@playwright/test')
+const {test, expect} = require('@playwright/test')
 
 test('Browser context Playwright test', async ({browser}) => {
 
     const context = await browser.newContext();
     const poge = await context.newPage();
+    // 暫時找不到可以測試的網頁
     await page.goto("https://playwright.dev/docs/test-timeouts");
+    console.log(await page.title());
+
+    // css      type, fill
+    // 現在不要用type
+    await page.locator('#username').fill("Yo");
+    await page.locator("[type='password']").fill("Yo!!");
+    await page.locator("#signInBtn").click();
     
 });
 
-test('Page Playwright test', async ({page}) => {
+test.only('Page Playwright test', async ({page}) => {
 
     await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
     
 });
