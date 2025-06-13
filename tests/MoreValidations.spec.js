46f93c85b63aef537af4962684d707b9fcdb24ac
MoriahYen <moriahyen@gmail.com>
2025-06-13 17:14:30 +0800

    43. How to automate Java/JavaScript Alert popups with Playwright

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -10,13 +10,14 @@ test('@Web Popup validations', async ({ page }) => {
     await expect(page.locator('#displayed-text')).toBeVisible();
     await page.locator('#hide-textbox').click();
     await expect(page.locator('#displayed-text')).toBeHidden();
-    
-    // await page.pause();
-    // page.on('dialog', dialog => dialog.accept());
-    // await page.locator('#confirmbtn').click();
-    // await page.locator('#mousehover').hover();
+
+    await page.pause();
+
+    page.on('dialog', dialog => dialog.accept()); //dismiss()
+    await page.locator('#confirmbtn').click();
+    await page.locator('#mousehover').hover();
     // const framesPage = page.frameLocator('#courses-iframe');
     // await framesPage.locator("li a[href*='lifetime-access']:visible").click();
-    // const textCheck = await framesPage.locator('.text h2').textContent();
+    // const textCheck = await framesPage.locator('.text h2').textContent();S
     // console.log(textCheck.split(' ')[1]);
 });
