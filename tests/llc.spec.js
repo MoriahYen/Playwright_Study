ca47438f04ad2085d5b2288f7d0b8692937b9a72
MoriahYen <moriahyen@gmail.com>
2025-06-13 11:12:51 +0800

    34. Filtering elements with GetByRole,GetByText and perform chaining methods in step

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -1,11 +1,15 @@
 const { test, expect } = require('@playwright/test');
 
 test('Play special locators', async ({ page }) => {
-    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.goto('https://rahulshettyacademy.com/angularpractice/');
     // playwright會搜尋整個zone(include"Check me out if you Love IceCreams!")
     // 直到找到click button
-    await page.getByLabel("Check me out if you Love IceCreams!").click();
-    await page.getByLabel("Employed").check();
-    await page.getByLabel("Gender").selectOption("Female");
-    
+    await page.getByLabel('Check me out if you Love IceCreams!').click();
+    await page.getByLabel('Employed').check();
+    await page.getByLabel('Gender').selectOption('Female');
+    await page.getByPlaceholder('Password').fill('abc123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await page.getByText('Success! The Form has been submitted successfully!.').isVisible();
+    await page.getByRole('link', { name: 'Shop' }).click();
+    await page.locator('app-card').filter({ hasText: 'Nokia Edge' }).getByRole('button').click();
 });
